b8329a0b96bd829b1be9f8c342f893b13ae9e734
Naman Vora <namanvr252@gmail.com>
2025-02-15 16:49:26 +0530

    first commit

---
+++ financial_agent.py
@@ -0,0 +1,53 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import openai
+import os
+
+
+os.environ["OPENAI_API_KEY"] = "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" #Does not exist anymore.
+
+import os
+
+api_key = os.getenv("GROQ_API_KEY")
+if not api_key:
+    raise EnvironmentError("GROQ_API_KEY not set. Please set the environment variable.")
+print(f"Using API key: {api_key[:5]}... (hidden for security)")
+
+
+## Web Search Agent
+web_search_agent = Agent(
+    name = "Web Search Agent",
+    role = "Search the web for the information",
+    model = Groq(id="llama3-8b-8192"),
+    tools = [DuckDuckGo()],
+    instructions = ("Always include sources"),
+    show_tool_calls = True,
+    markdown = True
+)
+
+
+## Financial Agent
+finance_agent = Agent(
+    name = "Finance AI Agent",
+    model = Groq(id="llama3-8b-8192"),
+    tools = 
+        [YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True, technical_indicators=True)]
+    ,
+    instructions=["Use tables to display the data"],
+    show_tool_calls = True,
+    markdown = True,
+
+)
+
+multi_ai_agent = Agent(
+    team = [web_search_agent,finance_agent],
+    model = Groq(id="llama3-8b-8192"),
+    instructions = ["Always include sources", "Use table to display the data"],
+    show_tool_calls = True,
+    markdown = True,
+)
+
+multi_ai_agent.update_model = "gpt-3.5-turbo"
+multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA", stream = True)
